6347914115667ce5f847ae726de12ea9e8a639b5
kate kiladis <kakladis@gmail.com>
2019-05-23 08:59:47 -0400

    fix slideshow

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,11 @@
 // Open the Modal
 function openModal() {
-  document.getElementById('myModal').style.display = "block";
+  document.getElementById("myModal").style.display = "block";
 }
 
 // Close the Modal
 function closeModal() {
-  document.getElementById('myModal').style.display = "none";
+  document.getElementById("myModal").style.display = "none";
 }
 
 var slideIndex = 1;
